cc57c47afce2f7838758cdb21b921f8a80440e2a
Tom-Brd <bourlard.tom.94@gmail.com>
2024-03-25 17:40:33 +0100

    init project

---
+++ src/main/java/org/pat/causeconnect/CauseconnectApplication.java
@@ -0,0 +1,13 @@
+package org.pat.causeconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CauseconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CauseconnectApplication.class, args);
+    }
+
+}
