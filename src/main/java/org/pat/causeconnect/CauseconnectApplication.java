fd1deba2bd29cbb20228c4085a92148a2f0f11c2
Tom-Brd <bourlard.tom.94@gmail.com>
2024-03-27 00:12:12 +0100

    feat(authentication): create forgot password modal and api call to send reset mail

--- src/main/java/org/pat/causeconnect/CauseconnectApplication.java
+++ src/main/java/org/pat/causeconnect/CauseconnectApplication.java
@@ -15,8 +15,6 @@ public class CauseconnectApplication implements AppShellConfigurator {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(CauseconnectApplication.class, args);
-
-        System.out.println(AssociationContext.getInstance().getAssociation().getId());
     }
 
 }
