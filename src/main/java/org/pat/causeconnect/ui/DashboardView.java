7ac5cf6b3053cc2b5fba55e66045a5e02fb54554
Tom-Brd <bourlard.tom.94@gmail.com>
2024-03-28 01:23:56 +0100

    feat(main-layout): create main layout and an empty dashboard view
    there is a problem with css theme configuration for now

--- src/main/java/org/pat/causeconnect/ui/DashboardView.java
+++ src/main/java/org/pat/causeconnect/ui/DashboardView.java
@@ -2,13 +2,16 @@ package org.pat.causeconnect.ui;
 
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.PermitAll;
 
 @Route(value = "", layout = MainLayout.class)
 @PermitAll
+@PageTitle("Dashboard")
 public class DashboardView extends VerticalLayout {
     public DashboardView() {
-        add(new H1("Dashboard View"));
     }
 }
--- src/main/java/org/pat/causeconnect/ui/MainLayout.java
+++ src/main/java/org/pat/causeconnect/ui/MainLayout.java
@@ -2,49 +2,113 @@ package org.pat.causeconnect.ui;
 
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.orderedlayout.Scroller;
+import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.server.auth.AccessAnnotationChecker;
 import com.vaadin.flow.theme.lumo.LumoUtility;
+import org.pat.causeconnect.entity.AssociationContext;
 import org.pat.causeconnect.service.SecurityService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.vaadin.lineawesome.LineAwesomeIcon;
+
+import java.util.Optional;
 
 public class MainLayout extends AppLayout {
-    private final SecurityService securityService;
+    private H2 viewTitle;
 
-    public MainLayout(SecurityService securityService) {
+    private final SecurityService securityService;
+    private final AccessAnnotationChecker accessChecker;
+
+    public MainLayout(SecurityService securityService, AccessAnnotationChecker accessChecker) {
         this.securityService = securityService;
+        this.accessChecker = accessChecker;
+        setPrimarySection(Section.DRAWER);
         createHeader();
         createDrawer();
     }
 
     private void createHeader() {
-        H1 logo = new H1("Cause Connect");
-        logo.addClassNames(
-                LumoUtility.FontSize.LARGE,
-                LumoUtility.Margin.MEDIUM);
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.setAriaLabel("Menu toggle");
 
-        String u = securityService.getAuthenticatedUser().getUsername();
-        Button logout = new Button("Log out " + u, e -> securityService.logout());
+        viewTitle = new H2();
+        viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
 
-        var header = new HorizontalLayout(new DrawerToggle(), logo, logout);
-
-        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
-        header.expand(logo);
-        header.setWidthFull();
-        header.addClassNames(
-                LumoUtility.Padding.Vertical.NONE,
-                LumoUtility.Padding.Horizontal.MEDIUM);
-
-        addToNavbar(header);
-
+        addToNavbar(true, toggle, viewTitle);
     }
 
     private void createDrawer() {
-        addToDrawer(new VerticalLayout(
-                new RouterLink("", DashboardView.class)
-        ));
+        String associationName = AssociationContext.getInstance().getAssociation().getName();
+        H1 appName = new H1(associationName);
+        appName.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+
+        Scroller scroller = new Scroller(createNavigation());
+
+        addToDrawer(appName, scroller, createFooter());
+    }
+
+    private SideNav createNavigation() {
+        SideNav nav = new SideNav();
+
+        if (accessChecker.hasAccess(DashboardView.class)) {
+            nav.addItem(new SideNavItem("Dashboard", DashboardView.class, LineAwesomeIcon.LIST_SOLID.create()));
+        }
+
+        return nav;
+    }
+
+    private Footer createFooter() {
+        Footer footer = new Footer();
+
+        Optional<UserDetails> user = securityService.getAuthenticatedUser();
+        if (user.isPresent()) {
+            UserDetails userDetails = user.get();
+
+            Avatar avatar = new Avatar(userDetails.getUsername());
+            avatar.setThemeName("xsmall");
+            avatar.getElement().setAttribute("tabindex", "-1");
+
+            MenuBar userMenu = new MenuBar();
+            userMenu.setThemeName("tertiary-inline contrast");
+
+            MenuItem userName = userMenu.addItem("");
+            Div div = new Div();
+            div.add(avatar);
+            div.add(userDetails.getUsername());
+            div.add(new Icon("lumo", "dropdown"));
+            div.getElement().getStyle().set("display", "flex");
+            div.getElement().getStyle().set("align-items", "center");
+            div.getElement().getStyle().set("gap", "var(--lumo-space-s)");
+            userName.add(div);
+            userName.getSubMenu().addItem("Déconnexion", e -> {
+                securityService.logout();
+                getUI().ifPresent(ui -> ui.navigate(LoginView.class));
+            });
+
+            footer.add(userMenu);
+        } else {
+            Anchor loginLink = new Anchor("login", "Sign in");
+            footer.add(loginLink);
+        }
+
+        return footer;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
     }
 }
