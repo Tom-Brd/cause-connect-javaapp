4bb719d8c88adb0166a28ea76e85cf44e7c95edb
Tom-Brd <bourlard.tom.94@gmail.com>
2024-04-01 17:16:25 +0200

    feat(authentication): create service to get all associations

---
+++ src/main/java/org/pat/causeconnect/service/AssociationService.java
@@ -0,0 +1,19 @@
+package org.pat.causeconnect.service;
+
+import org.pat.causeconnect.entity.Association;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class AssociationService {
+    @Value("${base.url}")
+    private String baseUrl;
+
+    public Association[] getAssociations() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = baseUrl + "/associations";
+
+        return restTemplate.getForObject(url, Association[].class);
+    }
+}
