ffb1987ffdf6bdada42de1c7f9259e62276cec27
Tom-Brd <bourlard.tom.94@gmail.com>
2024-04-01 17:51:54 +0200

    feat(CauseconnectApplication): remove useless code on application launch

--- src/main/java/org/pat/causeconnect/CauseconnectApplication.java
+++ src/main/java/org/pat/causeconnect/CauseconnectApplication.java
@@ -3,18 +3,10 @@ package org.pat.causeconnect;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.theme.Theme;
-import org.pat.causeconnect.entity.Association;
-import org.pat.causeconnect.entity.AssociationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Properties;
 
 @SpringBootApplication
 @Theme(value = "causeconnect")
@@ -22,37 +14,6 @@ import java.util.Properties;
 public class CauseconnectApplication implements AppShellConfigurator {
 
     public static void main(String[] args) throws IOException {
-        Properties appProps = PropertiesLoaderUtils.loadProperties(
-                new ClassPathResource("application.properties")
-        );
-        String baseUrl = appProps.getProperty("base.url");
-        if (baseUrl == null || baseUrl.isEmpty()) {
-            throw new RuntimeException("base.url property is not set in application.properties");
-        }
-
-        Properties prop = new Properties();
-
-        String propertiesFileName = "association.properties";
-        String externalPropertiesPath = Paths.get(System.getProperty("user.dir"), propertiesFileName).toString();
-        try {
-            FileInputStream ip = new FileInputStream(externalPropertiesPath);
-            prop.load(ip);
-
-            String associationId = prop.getProperty("association_id");
-
-            if (associationId == null || associationId.isEmpty()) {
-                throw new RuntimeException("Association ID not found in properties file");
-            }
-
-            RestTemplate restTemplate = new RestTemplate();
-            String url = baseUrl + "/associations/" + associationId;
-            Association association = restTemplate.getForObject(url, Association.class);
-
-            AssociationContext.getInstance().setAssociation(association);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
         SpringApplication.run(CauseconnectApplication.class, args);
     }
 
