0962bda28a9405e0427f2ef1c66e519305699a84
Tom-Brd <bourlard.tom.94@gmail.com>
2024-04-19 12:24:43 +0200

    fix: add service bean

--- src/main/java/org/pat/causeconnect/service/MainLayoutService.java
+++ src/main/java/org/pat/causeconnect/service/MainLayoutService.java
@@ -1,7 +1,9 @@
 package org.pat.causeconnect.service;
 
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MainLayoutService {
     private HorizontalLayout pluginContainer;
 
